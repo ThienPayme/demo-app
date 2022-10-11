dce3cfe5016077ef3eb5c163ac23450b48af3e6b
Pham Anh Thien <thienpa@MECORP.LOCAL>
2022-10-11 08:38:27 +0700

    test camera

--- pages/webcam-detection.tsx
+++ pages/webcam-detection.tsx
@@ -1,5 +1,4 @@
-
-import React from "react"
+import React from "react";
 import Webcam from "react-webcam";
 const WebcamPage = () => {
   const webcamRef = React.useRef<any>(null);
@@ -12,21 +11,17 @@ const WebcamPage = () => {
 
   return (
     <>
-       <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-      />
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <button onClick={capture}>Capture photo</button>
-      {imgSrc && (
-        <img
-          src={imgSrc}
-        />
-      )}
+      {imgSrc && <img src={imgSrc} />}
+
+      <div style={{ marginTop: 100 }}>
+        <input type="file" accept="image/*" capture />
+        <input type="file" accept="image/*" capture="user" />
+        <input type="file" accept="image/*" capture="environment" />
+      </div>
     </>
-     
+  );
+};
 
-  )
-}
-
-export default WebcamPage
+export default WebcamPage;
