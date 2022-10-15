8e9b4ac519da30e107735114f6bcc579ae65c8f2
phamanhthien1 <62327329+phamanhthien1@users.noreply.github.com>
2022-10-15 11:42:00 +0700

    update

--- pages/webcam-detection.tsx
+++ pages/webcam-detection.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import Webcam from "react-webcam";
+import { Upload } from "antd"
+import React from "react"
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import Webcam from "react-webcam"
 const WebcamPage = () => {
-  const webcamRef = React.useRef<any>(null);
-  const [imgSrc, setImgSrc] = React.useState(null);
+  const webcamRef = React.useRef<any>(null)
+  const [imgSrc, setImgSrc] = React.useState(null)
 
   const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-  }, [webcamRef, setImgSrc]);
-
+    const imageSrc = webcamRef.current.getScreenshot()
+    setImgSrc(imageSrc)
+  }, [webcamRef, setImgSrc])
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  )
   return (
     <>
       <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
@@ -21,9 +28,19 @@ const WebcamPage = () => {
         <input type="file" accept="image/*" capture="environment" />
       </div>
       <h1>QR Code Scanner</h1>
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        accept="image/*"
+
+
+      >
+        {uploadButton}
+      </Upload>
       <canvas id="qr-canvas"></canvas>
     </>
-  );
-};
+  )
+}
 
-export default WebcamPage;
+export default WebcamPage
